2af631d6ae93537d6e5224a959b7649a7b66ec03
10cheon00 <10cheon00@gmail.com>
2025-04-01 21:34:52 +0900

    chore: change venv folder name to .venv

--- codedeploy/install_dependencies.sh
+++ codedeploy/install_dependencies.sh
@@ -4,14 +4,13 @@ set -e
 # 배포 대상 디렉토리로 이동 (appspec.yml의 destination과 동일)
 cd /home/ubuntu/flask_test
 
-# 가상환경 생성 (필요 시 기존 환경 삭제)
-if [ -d "venv" ]; then
-  rm -rf venv
+# 가상환경 폴더가 존재하는지 확인, 없을 경우에만 새로 생성
+if [ ! -d ".venv" ]; then
+  python3 -m venv .venv
 fi
-python3 -m venv venv
 
 # 가상환경 활성화 후 의존성 설치
-source venv/bin/activate
+source .venv/bin/activate
 pip install --upgrade pip
 if [ -f requirements.txt ]; then
   pip install -r requirements.txt
