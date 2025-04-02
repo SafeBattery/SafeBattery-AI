f719a23a021be5a4052abb8754bc15602603a967
10cheon00 <10cheon00@gmail.com>
2025-04-02 14:17:16 +0900

    chore: zip with mysql secrets and run server after kill already running server

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -2,17 +2,19 @@
 cd /home/ubuntu/flask_test
 source .venv/bin/activate
 
-# 이전에 실행 중인 Gunicorn 프로세스가 있다면 종료 (PID 파일 사용 또는 pgrep로 검색)
-# 예시: PID 파일 방식으로 관리하는 경우
+# PID 파일에 기록된 gunicorn 프로세스가 있으면 강제 종료
 if [ -f gunicorn.pid ]; then
     PID=$(cat gunicorn.pid)
     if ps -p $PID > /dev/null; then
         echo "Stopping existing Gunicorn process with PID $PID"
-        kill $PID
+        sudo kill -9 $PID
         sleep 5
     fi
 fi
 
-# Gunicorn 실행 (백그라운드에서 실행하고 PID 기록)
+# PID 파일 기반으로 종료되지 않은 다른 gunicorn 프로세스들도 강제로 종료 (다른 유저의 프로세스 포함)
+sudo pkill -9 -f gunicorn
+
+# 새로운 gunicorn 프로세스 백그라운드 실행 및 PID 기록
 nohup gunicorn --workers 3 --bind 0.0.0.0:8000 flask_test:app > gunicorn.log 2>&1 &
 echo $! > gunicorn.pid
